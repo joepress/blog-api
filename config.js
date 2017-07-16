4b19e8c0eaa789b38085e6f7902651caf5cd3f26
joepress <jmpressley16@gmail.com>
2017-07-16 01:01:16 -0400

    figuring out heroku

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 exports.DATABASE_URL = process.env.DATABASE_URL ||
                        global.DATABASE_URL ||
                       'mongodb://localhost/blog-api';
-exports.PORT = process.env.PORT || 8080;
+exports.PORT = process.env.PORT ;
