a38ee31ee510361e0145778f3d2da3781bed17ca
joepress <jmpressley16@gmail.com>
2017-07-17 13:09:49 -0400

    working with heroku

--- server.js
+++ server.js
@@ -6,8 +6,8 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
 
-const {DATABASE_URL, PORT} = require('./config');
-const {BlogPost} = require('./models');
+const { DATABASE_URL, PORT } = require('./config');
+const { BlogPost } = require('./models');
 
 const app = express();
 
@@ -33,138 +33,138 @@ app.get('/blogposts', (req, res) => {
         ))
         .catch(err => {
             console.error(err);
-            res.status(500).json({message: 'Internal server error'});
+            res.status(500).json({ message: 'Internal server error' });
         });
 });
 
 // can also request by ID
 app.get('/blogposts/:id', (req, res) => {
-  BlogPost
-    // this is a convenience method Mongoose provides for searching
-    // by the object _id property
-    .findById(req.params.id)
-    .exec()
-    .then(blogpost =>res.json(blogpost.apiRepr()))
-    .catch(err => {
-      console.error(err);
-        res.status(500).json({message: 'Internal server error'});
-    });
+    BlogPost
+        // this is a convenience method Mongoose provides for searching
+        // by the object _id property
+        .findById(req.params.id)
+        .exec()
+        .then(blogpost => res.json(blogpost.apiRepr()))
+        .catch(err => {
+            console.error(err);
+            res.status(500).json({ message: 'Internal server error' });
+        });
 });
 
 
 app.post('/blogposts', (req, res) => {
 
-  const requiredFields = ['title', 'content', 'author'];
-  for (let i=0; i<requiredFields.length; i++) {
-    const field = requiredFields[i];
-    if (!(field in req.body)) {
-      const message = `Missing \`${field}\` in request body`;
-      console.error(message);
-      return res.status(400).send(message);
+    const requiredFields = ['title', 'content', 'author'];
+    for (let i = 0; i < requiredFields.length; i++) {
+        const field = requiredFields[i];
+        if (!(field in req.body)) {
+            const message = `Missing \`${field}\` in request body`;
+            console.error(message);
+            return res.status(400).send(message);
+        }
     }
-  }
 
-  BlogPost
-    .create({
-      title: req.body.title,
-      content: req.body.content,
-      author: req.body.author,
-      created: req.body.created,
-    })
-    .then(
-      blogpost => res.status(201).json(blogpost.apiRepr()))
-    .catch(err => {
-      console.error(err);
-      res.status(500).json({message: 'Internal server error'});
-    });
+    BlogPost
+        .create({
+            title: req.body.title,
+            content: req.body.content,
+            author: req.body.author,
+            created: req.body.created,
+        })
+        .then(
+            blogpost => res.status(201).json(blogpost.apiRepr()))
+        .catch(err => {
+            console.error(err);
+            res.status(500).json({ message: 'Internal server error' });
+        });
 });
 
 
 app.put('/blogposts/:id', (req, res) => {
-  // ensure that the id in the request path and the one in request body match
-  if (!(req.params.id && req.body.id && req.params.id === req.body.id)) {
-    const message = (
-      `Request path id (${req.params.id}) and request body id ` +
-      `(${req.body.id}) must match`);
-    console.error(message);
-    res.status(400).json({message: message});
-  }
-
-  // we only support a subset of fields being updateable.
-  // if the user sent over any of the updatableFields, we udpate those values
-  // in document
-  const toUpdate = {};
-  const updateableFields = ['title', 'content'];
-
-  updateableFields.forEach(field => {
-    if (field in req.body) {
-      toUpdate[field] = req.body[field];
+    // ensure that the id in the request path and the one in request body match
+    if (!(req.params.id && req.body.id && req.params.id === req.body.id)) {
+        const message = (
+            `Request path id (${req.params.id}) and request body id ` +
+            `(${req.body.id}) must match`);
+        console.error(message);
+        res.status(400).json({ message: message });
     }
-  });
 
-  BlogPost
-    // all key/value pairs in toUpdate will be updated -- that's what `$set` does
-    .findByIdAndUpdate(req.params.id, {$set: toUpdate})
-    .exec()
-    .then(blogpost => res.status(204).end())
-    .catch(err => res.status(500).json({message: 'Internal server error'}));
+    // we only support a subset of fields being updateable.
+    // if the user sent over any of the updatableFields, we udpate those values
+    // in document
+    const toUpdate = {};
+    const updateableFields = ['title', 'content'];
+
+    updateableFields.forEach(field => {
+        if (field in req.body) {
+            toUpdate[field] = req.body[field];
+        }
+    });
+
+    BlogPost
+        // all key/value pairs in toUpdate will be updated -- that's what `$set` does
+        .findByIdAndUpdate(req.params.id, { $set: toUpdate })
+        .exec()
+        .then(blogpost => res.status(204).end())
+        .catch(err => res.status(500).json({ message: 'Internal server error' }));
 });
 
-app.delete('/blog-posts/:id', (req, res) => {
-  BlogPost
-    .findByIdAndRemove(req.params.id)
-    .exec()
-    .then(blogpost => res.status(204).end())
-    .catch(err => res.status(500).json({message: 'Internal server error'}));
+app.delete('/blogposts/:id', (req, res) => {
+    BlogPost
+        .findByIdAndRemove(req.params.id)
+        .exec()
+        .then(blogpost => res.status(204).end())
+        .catch(err => res.status(500).json({ message: 'Internal server error' }));
 });
 
 // catch-all endpoint if client makes request to non-existent endpoint
 app.use('*', function(req, res) {
-  res.status(404).json({message: 'Not Found'});
+    res.status(404).json({ message: 'Not Found' });
 });
 
 let server;
 
 // this function connects to our database, then starts the server
-function runServer(databaseUrl=DATABASE_URL, port=PORT) {
+function runServer(databaseUrl = DATABASE_URL, port = PORT) {
 
-  return new Promise((resolve, reject) => {
-    mongoose.connect(databaseUrl, err => {
-      if (err) {
-        return reject(err);
-      }
-      server = app.listen(port, () => {
-        console.log(`Your app is listening on port ${port}`);
-        resolve();
-      })
-      .on('error', err => {
-        mongoose.disconnect();
-        reject(err);
-      });
+    return new Promise((resolve, reject) => {
+        mongoose.connect(databaseUrl, err => {
+            if (err) {
+                return reject(err);
+            }
+            server = app.listen(port, () => {
+                    console.log(`Your app is listening on port ${port}`);
+                    resolve();
+                })
+                .on('error', err => {
+                    mongoose.disconnect();
+                    reject(err);
+                });
+        });
     });
-  });
 }
 
 // this function closes the server, and returns a promise. we'll
 // use it in our integration tests later.
 function closeServer() {
-  return mongoose.disconnect().then(() => {
-     return new Promise((resolve, reject) => {
-       console.log('Closing server');
-       server.close(err => {
-           if (err) {
-               return reject(err);
-           }
-           resolve();
-       });
-     });
-  });
+    return mongoose.disconnect().then(() => {
+        return new Promise((resolve, reject) => {
+            console.log('Closing server');
+            server.close(err => {
+                if (err) {
+                    return reject(err);
+                }
+                resolve();
+            });
+        });
+    });
 }
 
 // if server.js is called directly (aka, with `node server.js`), this block
 // runs. but we also export the runServer command so other code (for instance, test code) can start the server as needed.
 if (require.main === module) {
-  runServer().catch(err => console.error(err));
+    runServer().catch(err => console.error(err));
 };
 
-module.exports = {app, runServer, closeServer};
+module.exports = { app, runServer, closeServer };
